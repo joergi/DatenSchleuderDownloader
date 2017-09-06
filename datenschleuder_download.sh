c0041500f4bdbf3f5afd1fdec6628ac87286dd71
Joergi <joergi@users.noreply.github.com>
2017-09-07 00:57:56 +0200

    Update datenschleuder_download.sh
    changes von sh to bash

--- datenschleuder_download.sh
+++ datenschleuder_download.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 # ------------------------------------------------------------------
-# [Author] joergi77 - https://github.com/joergi77
+# [Author] joergi - https://github.com/joergi/DatenSchleuderDownloader
 #          downloader for all DatenSchleuder issues
 #          they are downloadable for free under 
 #          http://ds.ccc.de/download.html
@@ -12,15 +12,12 @@
 #          this script is under GNU GENERAL PUBLIC LICENSE 
 # ------------------------------------------------------------------
 
-VERSION=0.1.0
-USAGE="Usage: sh datenschleuder_download.sh"
-
+VERSION=0.1.1
+USAGE="Usage: bash datenschleuder_download.sh"
 
 if [ ! -d "issues" ]; then
  mkdir issues
 fi
-
-
 
 # issues with special numbers
 wget http://ds.ccc.de/pdfs/ds0910.pdf -P issues/
@@ -29,7 +26,3 @@ wget http://ds.ccc.de/pdfs/ds044,5.pdf -P issues/
 
 #normal issues
 wget http://ds.ccc.de/pdfs/ds{002..096}.pdf -P issues/
-
-
-
-
