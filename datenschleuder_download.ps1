06acc9560d9891cb51252f7f10a15bd458e3d570
Joergi <joergi@users.noreply.github.com>
2017-10-18 00:34:53 +0200

    adding all pdfs

--- datenschleuder_download.ps1
+++ datenschleuder_download.ps1
@@ -27,7 +27,77 @@ if( -Not (Test-Path -Path "datenschleuder_issues" ) )
 }
 
 $datenschleuder_issues = @(
-	"https://ds.ccc.de/pdfs/ds096.pdf"
+	
+	"https://ds.ccc.de/pdfs/ds096.pdf",
+	"https://ds.ccc.de/pdfs/ds095.pdf",
+	"https://ds.ccc.de/pdfs/ds094.pdf",
+	"https://ds.ccc.de/pdfs/ds093.pdf",
+	"https://ds.ccc.de/pdfs/ds092.pdf",
+	"https://ds.ccc.de/pdfs/ds091.pdf",
+	"https://chaosradio.ccc.de/media/ds/ds090.pdf",
+	"https://chaosradio.ccc.de/media/ds/ds089.pdf",
+	"https://chaosradio.ccc.de/media/ds/ds088.pdf",
+	"https://chaosradio.ccc.de/media/ds/ds087.pdf",
+	"https://chaosradio.ccc.de/media/ds/ds086.pdf",
+	"https://chaosradio.ccc.de/media/ds/ds085.pdf",
+	"https://chaosradio.ccc.de/media/ds/ds084.pdf",
+	"https://chaosradio.ccc.de/media/ds/ds083.pdf",
+	"https://chaosradio.ccc.de/media/ds/ds082.pdf",
+	"https://chaosradio.ccc.de/media/ds/ds081.pdf",
+	"https://chaosradio.ccc.de/media/ds/ds079.pdf",
+	"https://chaosradio.ccc.de/media/ds/ds078.pdf",
+	"https://ds.ccc.de/pdfs/ds077.pdf",
+	"https://ds.ccc.de/pdfs/ds076.pdf",
+	"https://ds.ccc.de/pdfs/ds075.pdf",
+	"https://ds.ccc.de/pdfs/ds074.pdf",
+	"https://ds.ccc.de/pdfs/ds073.pdf",
+	"https://chaosradio.ccc.de/media/ds/ds072.pdf",
+	"https://chaosradio.ccc.de/media/ds/ds071.pdf",
+	"https://chaosradio.ccc.de/media/ds/ds070.pdf",
+	"https://chaosradio.ccc.de/media/ds/ds067.pdf",
+	"https://chaosradio.ccc.de/media/ds/ds066.pdf",
+	"https://chaosradio.ccc.de/media/ds/ds065.pdf",
+	"https://chaosradio.ccc.de/media/ds/ds064.pdf",
+	"https://chaosradio.ccc.de/media/ds/ds063.pdf",
+	"https://chaosradio.ccc.de/media/ds/ds062.pdf",
+	"https://chaosradio.ccc.de/media/ds/ds061.pdf",
+	"https://chaosradio.ccc.de/media/ds/ds060.pdf",
+	"https://chaosradio.ccc.de/media/ds/ds059.pdf",
+	"https://chaosradio.ccc.de/media/ds/ds058.pdf",
+	"https://ds.ccc.de/pdfs/ds057.pdf",
+	"https://ds.ccc.de/pdfs/ds056.pdf",
+	"https://ds.ccc.de/pdfs/ds053.pdf",
+	"https://ds.ccc.de/pdfs/ds048.pdf",
+	"https://ds.ccc.de/pdfs/ds047.pdf",
+	"https://ds.ccc.de/pdfs/ds046.pdf",
+	"https://ds.ccc.de/pdfs/ds045.pdf",
+	"https://ds.ccc.de/pdfs/ds044,5.pdf",
+	"https://ds.ccc.de/pdfs/ds044.pdf",
+	"https://ds.ccc.de/pdfs/ds043.pdf",
+	"https://ds.ccc.de/pdfs/ds042.pdf",
+	"https://ds.ccc.de/pdfs/ds041.pdf",
+	"https://ds.ccc.de/pdfs/ds040.pdf",
+	"https://ds.ccc.de/pdfs/ds039.pdf",
+	"https://ds.ccc.de/pdfs/ds038.pdf",
+	"https://ds.ccc.de/pdfs/ds037.pdf",
+	"https://ds.ccc.de/pdfs/ds036.pdf",
+	"https://ds.ccc.de/pdfs/ds035.pdf",
+	"https://ds.ccc.de/pdfs/ds034.pdf",
+	"https://ds.ccc.de/pdfs/ds033.pdf",
+	"https://ds.ccc.de/pdfs/ds032.pdf",
+	"https://ds.ccc.de/pdfs/ds031.pdf",
+	"https://ds.ccc.de/pdfs/ds030.pdf",
+	"https://ds.ccc.de/pdfs/ds016.pdf",
+	"https://ds.ccc.de/pdfs/ds015.pdf",
+	"https://ds.ccc.de/pdfs/ds014.pdf",
+	"https://ds.ccc.de/pdfs/ds013.pdf",
+	"https://ds.ccc.de/pdfs/ds1112.pdf",
+	"https://ds.ccc.de/pdfs/ds0910.pdf",
+	"https://ds.ccc.de/pdfs/ds008.pdf",
+	"https://ds.ccc.de/pdfs/ds007.pdf",
+	"https://ds.ccc.de/pdfs/ds0506.pdf",
+	"https://ds.ccc.de/pdfs/ds004.pdf",
+	"https://ds.ccc.de/pdfs/ds003.pdf"
 	
 )
 
@@ -41,8 +111,9 @@ try
 	    Write-Host $issue
 
 	    $issue_name = $issue
-	    # $issue_name = $issue_name.Replace("https://ds.ccc.de/pdfs/","")
-	    $issue_name.Replace("https://ds.ccc.de/pdfs/","")
+	    $issue_name = $issue_name.Replace("https://ds.ccc.de/pdfs/","")
+	    $issue_name = $issue_name.Replace("https://chaosradio.ccc.de/media/ds/","")
+	    $issue_name = $issue_name.Replace(",","_")
 
 	    $web.DownloadFile($issue, "$PSScriptRoot\datenschleuder_issues\\$issue_name")
 	} Catch
